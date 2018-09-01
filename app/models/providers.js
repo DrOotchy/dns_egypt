16c2567be59b41162c98ab368952e0a118131ad2
hderaz <h.deraz@teknowl.net>
2018-09-01 15:54:54 +0200

    Models Update

--- app/models/providers.js
+++ app/models/providers.js
@@ -15,4 +15,35 @@ const serviceProvidersSchema = mongoose.Schema( {
    
 });
 
-module.exports = mongoose.model("Providers", serviceProvidersSchema);
+const Providers = module.exports = mongoose.model("Providers", serviceProvidersSchema);
+
+// Get Providers
+module.exports.getProviders = (callback, limit) => {
+	Providers.find(callback).limit(limit);
+}
+
+// Get Providers
+module.exports.getProvidersById = (id, callback) => {
+	Providers.findById(id, callback);
+}
+
+// Add Providers
+module.exports.addProviders = (Providers, callback) => {
+	Providers.create(Providers, callback);
+}
+
+// Update Providers
+module.exports.updateProviders = (id, Providers, options, callback) => {
+	var query = {_id: id};
+	var update = {
+        name: Providers.name,
+        url: Providers.url
+	}
+	Providers.findOneAndUpdate(query, update, options, callback);
+}
+
+// Delete Providers
+module.exports.removeProviders = (id, callback) => {
+	var query = {_id: id};
+	Providers.remove(query, callback);
+}
--- app/models/tlds.js
+++ app/models/tlds.js
@@ -13,4 +13,35 @@ const topLevelDomainSchema = mongoose.Schema( {
 
 });
 
-module.exports = mongoose.model("TLDs", topLevelDomainSchema);
+const Tlds = module.exports = mongoose.model("TLDs", topLevelDomainSchema);
+
+// Get Tlds
+module.exports.getTlds = (callback, limit) => {
+	Tlds.find(callback).limit(limit);
+}
+
+// Get Tlds
+module.exports.getTldsById = (id, callback) => {
+	Tlds.findById(id, callback);
+}
+
+// Add Tlds
+module.exports.addTlds = (Tlds, callback) => {
+	Tlds.create(Tlds, callback);
+}
+
+// Update Tlds
+module.exports.updateTlds = (id, Tlds, options, callback) => {
+	var query = {_id: id};
+	var update = {
+        name: Tlds.name,
+        providerId: Tlds.providerId
+	}
+	Tlds.findOneAndUpdate(query, update, options, callback);
+}
+
+// Delete Tlds
+module.exports.removeTlds = (id, callback) => {
+	var query = {_id: id};
+	Tlds.remove(query, callback);
+}
